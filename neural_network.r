09bad569252b352b45491afd6f57068e08ae18d1
Nicolas Béreux <nicolas.bereux@gmail.com>
2021-02-24 17:07:51 +0100

    factorisation

---
+++ neural_network.r
@@ -0,0 +1,40 @@
+neural_network = function(train, test, plt = FALSE){
+    labels = train$Load
+    train.lstm = train[,-c(1,2,22,23)]
+    test.lstm = test[,-c(1,20,21,23,24)]
+
+    lstm = function(train_set, train_label, test_set) {
+        y = train_label
+        x = data.matrix(train_set)
+        x_test = data.matrix(test_set)
+        model = keras_model_sequential() %>%
+            layer_dense(units = 19, activation = 'relu', input_shape = c(19)) %>%
+            layer_dense(units = 12, activation = 'relu') %>%
+            layer_dense(units = 6, activation = 'relu') %>%
+            layer_dense(units=1, activation = "linear")
+        
+        model %>% compile(loss = 'mse',
+                          optimizer = optimizer_adam(0.0005))
+        
+        model %>% fit(x,y, epochs=15)
+
+        pred.train = model %>% predict(x)
+        pred.test = model %>% predict(x_test)
+
+        return(list("train"=pred.train,"test"=pred.test))
+    }
+
+
+    pred.lstm = lstm(train.lstm, labels, test.lstm)
+
+    if (plt){
+        par(mfrow=c(1,1))
+        plot(train$Load,type='l', xlim=c(0,length(total.time)))
+        lines(test$time,pred.lstm$test, col='green', lwd=1)
+    }
+
+    N = length(test$Load.1)
+    RMSE = rmse(pred.lstm$test[-N], test$Load.1[2:N])
+    RMSE
+    return pred.lstm
+}
---
+++ random_forest.r
@@ -0,0 +1,17 @@
+random_forest = function(train, test, plt = FALSE){
+
+    rf = randomForest(Load ~ ., data=train, mtry=3,
+                      importance=TRUE, na.action=na.omit)
+    pred.test.rf = predict(rf,test)
+
+    if (plt){
+        par(mfrow=c(1,1))
+        plot(train$Load,type='l', xlim=c(0,length(total.time)))
+        lines(test$time,pred.test.rf, col='green', lwd=1)
+    }
+
+    N = length(test$Load.1)
+    RMSE = rmse(pred.test.rf[-N], test$Load.1[2:N])
+    RMSE
+    return(pred.test.rf)
+ }
