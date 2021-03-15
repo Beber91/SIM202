650ed1b273d8c9883fed97211f1e7b2aed8f7662
Nicolas Béreux <nicolas.bereux@gmail.com>
2021-03-15 15:09:43 +0100

    package

--- comp_rapp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-Rscript -e "library(knitr); knit('rapport.Rnw')"
-pdflatex rapport.tex
--- expertagreg.r
+++
@@ -1,4 +0,0 @@
-add_expert = function(list_experts_train){
-    experts.train = array_reshape(abind(list_experts_train), c(3028,1,length(list_experts_train)/3028))
-    return(experts.train)
-}
